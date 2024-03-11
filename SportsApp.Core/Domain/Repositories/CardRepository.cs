49af561b05dc9c7eb2c238fbddadd781716329d2
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-03-11 03:58:44 +0300

    repositorywork

---
+++ SportsApp.Core/Domain/Repositories/CardRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportsApp.Core.Domain.RepositoryContracts;
+using SportsApp.Infrastructure.Data.Player;
+using System;
+using System.Collections.Generic;
+
+namespace SportsApp.Core.Doamain.Repositories {
+    public class CardRepository : ICardRepository {
+        public Task<CardEntity> Add(CardEntity entity) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SportsApp.Infrastructure/Repositories/PassRepository.cs
+++ SportsApp.Infrastructure/Repositories/PassRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SportsApp.Infrastructure.Repositories {
-    public class GameRepository {
+    internal class PassRepository {
     }
 }
