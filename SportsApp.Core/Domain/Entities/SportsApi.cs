44588144ddde9e46c8e53a2d9fd08122e13f5bf7
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-03-16 12:23:57 +0300

    query builder lookup

---
+++ SportsApp.Core/Domain/Entities/SportsApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SportsApp.Core.Domain.Entities {
+    public class SportsApi {
+        public class Players {
+            public string? Id { get; set; }
+            public string? TeamId { get; set; }
+            public string? LeagueId { get; set; }
+            public string? Search { get; set; }
+            public string? Season { get; set; }
+        }
+    }
+}
